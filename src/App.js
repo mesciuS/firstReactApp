bea92a85a210a6ae7f3647c5e7cfe5c2493e8a0e
Giovanni <giovadev@gmail.com>
2023-10-24 17:43:32 +0200

    feat: card assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Heading from './components/Heading.js';
+import Card from './components/Card.js';
 
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
       {/* i components con proprs devono stare dentro un div */}
       <Heading  firstName="Anna" />
       <Heading firstName="Giacomo" />
+      <h1>Task: Add three card elements </h1>
+      <Card h2="First card's h2" h3="First card's h3"/>
+      <Card h2="Second card's h2" h3="Second card's h3"/>
+      <Card h2="Third card's h2" h3="Third card's h3"/>
     </div>
   );
 }
